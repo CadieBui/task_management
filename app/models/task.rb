a8edfba015c5854785fe95769bc66ca0797f50ab
Cadie <candybui61@gmail.com>
2022-10-14 19:47:19 +0800

    Fix: Make ab better code

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, :presence => true,:length => { :minimum => 5 }
-  validates :content,  :presence => true,:length => { :minimum => 5 }
+  validates :title, :presence => true,:length => { :minimum => 5 }, uniqueness: true
+  validates :content,  :presence => true,:length => { :minimum => 5 }, uniqueness: true
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -7,26 +7,23 @@ RSpec.describe "Tasks", type: :system do
   
   # success case
   scenario "valid title and content" do  
-    visit tasks_path
-    click_link "New"
-    fill_in 'Title', with: Faker::Lorem.sentence 
-    fill_in 'Content', with: Faker::Lorem.sentence 
-    click_button 'Submit'
-    expect(page).to have_content("Task was successfully created")
+    if Task.exists?(title: Faker::Lorem.sentence) || Task.exists?(content: Faker::Lorem.sentence)
+      run_create_task(title: Faker::Lorem.sentence(word_count: 10), content: Faker::Lorem.sentence(word_count: 10))
+      expect(page).to have_content("Task was successfully created")
+    else
+      run_create_task(title: Faker::Lorem.sentence, content: Faker::Lorem.sentence)
+      expect(page).to have_content("Task was successfully created")
+    end
   end
 
   # error case
   scenario "empty title and content" do  
-    visit tasks_path
-    click_link "New"
-    fill_in 'Title', with: ''
-    fill_in 'Content', with: ''
-    click_button 'Submit'
+    run_create_task(title: '', content: '')
     expect(page).to have_content("Title can't be blank Title is too short (minimum is 5 characters) Content can't be blank Content is too short (minimum is 5 characters)")
   end
 
   # error case
-  scenario "empty title " do      
+  scenario "empty title " do   
     visit tasks_path
     click_link "New"
     fill_in 'Title', with: ''
@@ -42,4 +39,14 @@ RSpec.describe "Tasks", type: :system do
     click_button 'Submit'
     expect(page).to have_content("Content can't be blank Content is too short (minimum is 5 characters)")
   end
+
+  private
+    def run_create_task(title:, content:)
+      visit tasks_path
+      click_link "New"
+      fill_in 'Title', with: title
+      fill_in 'Content', with: content
+      click_button 'Submit'
+    end
+
 end
