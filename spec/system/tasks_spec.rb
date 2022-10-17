a1f75bd9214ba46ec044cdb08a037997b2640ba8
Cadie <candybui61@gmail.com>
2022-10-17 18:47:14 +0800

    Fix: Resolve seperate test case

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,49 +6,59 @@ RSpec.describe "Tasks", type: :system do
     Task.destroy_all
   end
 
-  # success case
-  scenario "valid title and content" do  
+  # success case in create
+  scenario "valid title and content in create" do  
     run_create_task(title: Faker::Lorem.sentence, content: Faker::Lorem.sentence)
-
     expect(page).to have_content("Task was successfully created")
   end
 
-  # success case
-  scenario "valid title and content on edit" do  
+  # success case in edit
+  scenario "valid title and content in edit" do  
     run_edit_task(title: Faker::Lorem.sentence, content: Faker::Lorem.sentence)
-
     expect(page).to have_content("Task was successfully updated")
   end
 
-  # success case
+  # success case in delete
   scenario "deleted successfully" do  
     run_delete_task
     expect(page).to have_content("Task was successfully destroyed")
   end
 
-  # error case
-  scenario "empty title and content" do  
-    run_create_task(title: '', content: '')
+  # error case in create
+  scenario "empty title and content in create" do  
     run_edit_task(title:'', content: '')
-
     expect(page).to have_content("Title can't be blank Title is too short (minimum is 5 characters) Content can't be blank Content is too short (minimum is 5 characters)")
   end
 
-  # error case
-  scenario "empty title " do       
+  # error case in create
+  scenario "empty title in create " do       
     run_create_task(title:'', content: Faker::Lorem.sentence)
-    run_edit_task(title:'', content: Faker::Lorem.sentence)
-
     expect(page).to have_content("Title can't be blank Title is too short (minimum is 5 characters)")
   end
-  
-  # error case
-  scenario "empty content " do  
-    run_create_task(title: Faker::Lorem.sentence, content: '')
-    run_edit_task(title: Faker::Lorem.sentence, content: '')
 
+  # error case in create
+  scenario "empty content in create " do  
+    run_create_task(title: Faker::Lorem.sentence, content: '')
     expect(page).to have_content("Content can't be blank Content is too short (minimum is 5 characters)")
   end
+
+  # error case in edit
+  scenario "empty title and content in edit" do  
+    run_edit_task(title:'', content: '')
+    expect(page).to have_content("Title can't be blank Title is too short (minimum is 5 characters) Content can't be blank Content is too short (minimum is 5 characters)")
+  end
+
+  # error case in edit
+  scenario "empty title in edit " do       
+    run_edit_task(title:'', content: Faker::Lorem.sentence)
+    expect(page).to have_content("Title can't be blank Title is too short (minimum is 5 characters)")
+  end
+
+  # error case in edit
+  scenario "empty content in edit " do  
+    run_edit_task(title: Faker::Lorem.sentence, content: '')
+    expect(page).to have_content("Content can't be blank Content is too short (minimum is 5 characters)")
+  end 
 
   private
     # test create task
