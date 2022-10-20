3cff24e8d84eea5a92eb4eaac812582a3c3496de
Cadie <candybui61@gmail.com>
2022-10-20 20:35:51 +0800

    Feat: Resolve issue#17

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -7,19 +7,41 @@ RSpec.describe "Tasks", type: :system do
   end
 
   # success case in create
-  scenario "valid title and content in create" do  
+  scenario "valid title and content with endtime in create" do  
     title = Faker::Lorem.sentence
     content = Faker::Lorem.sentence
-    run_create_task(title: title, content: content)
+    endtime = Time.now
+    run_create_task(title: title, content: content, endtime: endtime)
+    expect(page).to have_content I18n.t('forms.create.success')
+    expect(Task.find_by(title: title, content: content)).to be_present
+  end
+
+  # success case in create
+  scenario "valid title and content without endtime in create" do  
+    title = Faker::Lorem.sentence
+    content = Faker::Lorem.sentence
+    endtime = ""
+    run_create_task(title: title, content: content, endtime: endtime)
     expect(page).to have_content I18n.t('forms.create.success')
     expect(Task.find_by(title: title, content: content)).to be_present
   end
 
   # success case in edit
-  scenario "valid title and content in edit" do  
+  scenario "valid title and content with endtime in edit" do  
     title = Faker::Lorem.sentence
     content = Faker::Lorem.sentence
-    run_edit_task(title: title, content: content)
+    endtime = Time.now
+    run_edit_task(title: title, content: content, endtime: endtime)
+    expect(page).to have_content I18n.t('forms.edit.success')
+    expect(Task.find_by(title: title, content: content)).to be_present
+  end
+
+  # success case in edit
+  scenario "valid title and content without endtime in edit" do  
+    title = Faker::Lorem.sentence
+    content = Faker::Lorem.sentence
+    endtime = ""
+    run_edit_task(title: title, content: content, endtime: endtime)
     expect(page).to have_content I18n.t('forms.edit.success')
     expect(Task.find_by(title: title, content: content)).to be_present
   end
@@ -32,14 +54,14 @@ RSpec.describe "Tasks", type: :system do
 
   # error case in create
   scenario "empty title and content in create" do  
-    run_edit_task(title:'', content: '')
+    run_edit_task(title:'', content: '', endtime: '')
     expect(page).to have_content("error")
   end
 
   # error case in create
   scenario "empty title in create " do       
     content = Faker::Lorem.sentence
-    run_create_task(title:'', content: content)
+    run_create_task(title:'', content: content, endtime: Time.current)
     expect(page).to have_content('error')
 
   end
@@ -47,46 +69,47 @@ RSpec.describe "Tasks", type: :system do
   # error case in create
   scenario "empty content in create " do  
     title = Faker::Lorem.sentence
-    run_create_task(title: title, content: '')
+    run_create_task(title: title, content: '', endtime: Time.current)
     expect(page).to have_content("error")
-
   end
 
   # error case in edit
   scenario "empty title and content in edit" do  
-    run_edit_task(title:'', content: '')
+    run_edit_task(title:'', content: '', endtime: '')
     expect(page).to have_content("error")
   end
 
   # error case in edit
   scenario "empty title in edit " do       
     content = Faker::Lorem.sentence
-    run_edit_task(title:'', content: content)
+    run_edit_task(title:'', content: content, endtime: '')
     expect(page).to have_content("error")
   end
 
   # error case in edit
   scenario "empty content in edit " do  
     title = Faker::Lorem.sentence
-    run_edit_task(title: title, content: '')
+    run_edit_task(title: title, content: '', endtime: '')
     expect(page).to have_content("error")
   end 
 
   private
     # test create task
-    def run_create_task(title:, content:)
+    def run_create_task(title:, content:, endtime:)
       visit new_task_path
       fill_in I18n.t('forms.field_label.title'), with: title
       fill_in I18n.t('forms.field_label.content'), with: content
+      fill_in I18n.t('forms.field_label.endtime'), with: endtime
       click_button I18n.t('forms.button.submit')
     end
 
     # test edit task
-    def run_edit_task(title:, content:)
+    def run_edit_task(title:, content:, endtime:)
       task = create(:task)
       visit(edit_task_path(task.id))
       fill_in I18n.t('forms.field_label.title'), with: title
       fill_in I18n.t('forms.field_label.content'), with: content
+      fill_in I18n.t('forms.field_label.endtime'), with: endtime
       click_button I18n.t('forms.button.submit')
     end
 
