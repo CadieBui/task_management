1548272ff939d3e36557460afdee5ddf8ed916a9
Cadie <candybui61@gmail.com>
2022-10-26 16:55:51 +0800

    Fix: Resolve rspec error

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,4 @@
 class Task < ApplicationRecord
-  include TranslateEnum
-
   validates :title, :presence => true,:length => { :minimum => 5 }, uniqueness: true
   validates :content,  :presence => true,:length => { :minimum => 5 }
 
