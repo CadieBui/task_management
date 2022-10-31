36d5d16b5f86da242bd20c9ea87a1b29c087514a
Cadie <candybui61@gmail.com>
2022-11-01 01:59:45 +0800

    Feat: Resolve issue#27

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_password :recovery_password, validations: false
 
-  has_many :task
+  has_many :task, dependent: :destroy
 
   validates :username, :presence => true, uniqueness: true
   validates :password,  :presence => true
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
   resources :sessions
-  get 'login', to: "sessions#new"
-  post 'login', to: "sessions#create"
-
-  delete 'logout', to: "sessions#destroy"
-
   resources :users
   resources :tasks
+
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+
+  get 'admin', to: "users#index"
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "signup",to: "users#new"
   post "signup",to: "users#create"
 
-  # Defines the root path route ("/")
   root "tasks#index"
 end
